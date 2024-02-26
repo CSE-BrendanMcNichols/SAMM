530f08bd12f7e064b5e6e97844c8cf61ba734902
Sree <you@example.com>
2024-02-25 23:14:37 -0500

    Fixed the merged code

--- backEnd/DataLoader.java
+++ backEnd/DataLoader.java
@@ -11,38 +11,11 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;;
 
-public class DataLoader {
+public class DataLoader extends DataConstants {
 
 	public static ArrayList<Student> loadStudents() {
 		ArrayList<Student> students = new ArrayList<Student>();
-<<<<<<< HEAD
 
-		try {
-			FileReader reader = new FileReader(DataConstants.STUDENT_FILE_NAME);
-			JSONParser parser = new JSONParser();
-			JSONArray studentsJSON = (JSONArray) new JSONParser().parse(reader);
-
-			for (int i = 0; i < studentsJSON.size(); i++) {
-				JSONObject studentJSON = (JSONObject) studentsJSON.get(i);
-				String username = (String) studentJSON.get(DataConstants.USERNAME);
-				String password = (String) studentJSON.get(DataConstants.PASSWORD);
-				String email = (String) studentJSON.get(DataConstants.EMAIL);
-				String uscid = (String) studentJSON.get(DataConstants.USCID);
-				String uuid = (String) studentJSON.get(DataConstants.UUID);
-				String gradeYear = (String) studentJSON.get(DataConstants.GRADEYEAR);
-				String advisor = (String) studentJSON.get(DataConstants.ADVISOR);
-				String major = (String) studentJSON.get(DataConstants.MAJOR);
-				String overallGrade = (String) studentJSON.get(DataConstants.OVERALLGRADE);
-				String credits = (String) studentJSON.get(DataConstants.CREDITS);
-				String completedClasses = (String) studentJSON.get(DataConstants.COMPLETEDCLASSES);
-				String currentClasses = (String) studentJSON.get(DataConstants.GRADEYEAR);
-
-				students.add(new Student(username, password, email, uscid));
-			}
-
-			return students;
-
-=======
 		
 		try {
 			FileReader reader = new FileReader(STUDENT_FILE_NAME);
@@ -72,7 +45,6 @@ public class DataLoader {
 			
 			return students;
 			
->>>>>>> 161964660c18f96dd07cc7f551cc9c42056204f8
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -80,3 +52,4 @@ public class DataLoader {
 		return null;
 	}
 }
+//Made no changes
