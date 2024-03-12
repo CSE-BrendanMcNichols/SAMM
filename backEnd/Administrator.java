ceb9578a3e0295de855551d189cb4d53dcf4bc2b
Ankit Nath <ankitnath2004@gmail.com>
2024-03-12 12:52:36 -0400

    added to ticket #247

--- backEnd/Administrator.java
+++ backEnd/Administrator.java
@@ -1,8 +1,11 @@
 package backEnd;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class Administrator extends User {
+
+    Student student;
 
     public Administrator(String username, String password, String email, String uscid, UUID uuid) {
         super(username, password, email, uscid, uuid);
@@ -14,6 +17,22 @@ public class Administrator extends User {
 
     private void updateMajor(String majorDetails) {
         System.out.println("The new major is " + majorDetails);
+    }
+
+    private void overrideCourseReq(Course course, ArrayList<Course> newPreReq) {
+        //TODO implement logic
+    }
+
+    public boolean riskOfFaliure(Student student) {
+        if(student.riskOfFailure()) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    private void createCourse(String courseName,Semester courseSemester, int courseNumber, ArrayList<Requirement> prerequisites, ArrayList<Requirement> corequisites, String courseDescription) {
+        //TODO add logic into create Course
     }
 
 }
