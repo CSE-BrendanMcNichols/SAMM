468a35084059184d5bb5eecd7879dc551a6afc68
CSE-BrendanMcNichols <mcnichob@email.sc.edu>
2024-03-13 17:10:47 -0400

    #26 and #28 complete

--- backEnd/CourseList.java
+++ backEnd/CourseList.java
@@ -7,20 +7,11 @@ import java.util.UUID;
 public class CourseList {
     private ArrayList<Course> courses;
     private HashMap<UUID, Course> coursesByUuid;
-    private HashMap<String, UUID> uuidsByNameAndNumber;
+    private HashMap<String, UUID> uuidsBySubjectAndNumber;
     private static CourseList courseList;
 
     private CourseList() {
         courses = DataLoader.getCourses();
-        coursesByUuid = new HashMap<>();
-        uuidsByNameAndNumber = new HashMap<>();
-        
-        for (Course course : courses) {
-            UUID uuid = course.getUuid();
-            String key = course.getName() + " " + course.getCourseNumber();
-            coursesByUuid.put(uuid, course);
-            uuidsByNameAndNumber.put(key, uuid);
-        }
     }
 
     public static CourseList getInstance() {
@@ -30,25 +21,53 @@ public class CourseList {
         return courseList;
     }
 
-    public Course getCourseByUuid(UUID uuid) {
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    public Course getCourse(UUID uuid) {
         return coursesByUuid.get(uuid);
     }
 
-    public Course getCourseByNameAndNumber(String name, int number) {
-        UUID uuid = uuidsByNameAndNumber.get(name + " " + number);
+    public Course getCourse(String subject, int number) {
+        UUID uuid = uuidsBySubjectAndNumber.get(subject + " " + number);
         return coursesByUuid.get(uuid);
     }
 
-    public boolean addCourse(Course course) {
-        String key = course.getName() + " " + course.getCourseNumber();
-        if (!uuidsByNameAndNumber.containsKey(key)) {
+    public boolean createCourse(Course course) {
+        if (!coursesByUuid.containsKey(course.getUuid())) {
             courses.add(course);
-            UUID uuid = course.getUuid();
-            coursesByUuid.put(uuid, course);
-            uuidsByNameAndNumber.put(key, uuid);
+            coursesByUuid.put(course.getUuid(), course);
+            uuidsBySubjectAndNumber.put(course.getCourseSubject() + " " + course.getCourseNumber(), course.getUuid());
             return true;
         } else {
             return false;
         }
+    }
+
+    public boolean deleteCourse(Course course) {
+        if (courses.remove(course)) {
+            coursesByUuid.remove(course.getUuid());
+            uuidsBySubjectAndNumber.remove(course.getCourseSubject() + " " + course.getCourseNumber());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean editCourse(Course course) {
+        if (!coursesByUuid.containsKey(course.getUuid())) {
+            return false;
+        }
+
+        Course originalCourse = coursesByUuid.get(course.getUuid());
+        courses.remove(originalCourse);
+        uuidsBySubjectAndNumber.remove(originalCourse.getCourseSubject() + " " + originalCourse.getCourseNumber());
+
+        courses.add(course);
+        coursesByUuid.put(course.getUuid(), course);
+        uuidsBySubjectAndNumber.put(course.getCourseSubject() + " " + course.getCourseNumber(), course.getUuid());
+
+        return true;
     }
 }
