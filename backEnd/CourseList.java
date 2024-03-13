813e2d96cdb9968ca514b90e8f0d1d8dc193b1aa
CSE-BrendanMcNichols <mcnichob@email.sc.edu>
2024-03-12 21:11:28 -0400

    finished #28 CourseList

--- backEnd/CourseList.java
+++ backEnd/CourseList.java
@@ -1,50 +1,62 @@
+package backEnd;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class CourseList {
     private ArrayList<Course> courses;
+    private HashMap<UUID, Course> coursesByUuid;
+    private HashMap<String, UUID> uuidsByName;
     private static CourseList courseList;
 
     private CourseList() {
-      loadCourses();
+        courses = DataLoader.getCourses();
+        coursesByUuid = new HashMap<>();
+        uuidsByName = new HashMap<>();
+        
+        for (Course course : courses) {
+            UUID uuid = course.getUuid();
+            String key = course.getName(); 
+            coursesByUuid.put(uuid, course);
+            uuidsByName.put(key, uuid);
+        }
     }
 
     public static CourseList getInstance() {
-        if(courseList == null) {
+        if (courseList == null) {
             courseList = new CourseList();
         }
         return courseList;
     }
 
     public Course getCourse(String name) {
-        for (Course course : courses) {
-          if (course.getCourseName().equals(name)) {
-            return course;
-          }
+        UUID uuid = uuidsByName.get(name);
+        return coursesByUuid.get(uuid);
+    }
+
+    public boolean addCourse(Course course) {
+        if (!uuidsByName.containsKey(course.getName())) {
+            courses.add(course);
+            UUID uuid = course.getUuid();
+            String key = course.getName(); // Use course name as the key
+            coursesByUuid.put(uuid, course);
+            uuidsByName.put(key, uuid);
+            return true;
+        } else {
+            return false;
         }
-        return null;
-      }
+    }
 
-      public void addCourse(String name, Semester courseSemester, int courseNumber,  
-                            ArrayList<Requirement> preRequisites, ArrayList<Requirement> coRequisites,
-                            String courseDescription)
-                        if (name = true   
-                        || courseSemester == null
-                        || courseNumber == null
-                        || preRequisites == null
-                        || coRequisites == null
-                        || courseDescription == null
-                        || type == null);
-
-      public Course getCourseID(String id) {
-          for (Course course: courses) {
-            if(course.getCourseID().equals(id)) {
-              return course;
-            }
-          }
-      }
-
-      private void loadCourses() {
-        this.courses = DataLoader.getInstance().LoadCourses();
-      }
-}
+    public boolean deleteCourse(Course course) {
+        if (courses.remove(course)) {
+            UUID uuid = course.getUuid();
+            String key = course.getName(); // Use course name as the key
+            coursesByUuid.remove(uuid);
+            uuidsByName.remove(key);
+            return true;
+        } else {
+            return false;
+        }
+    }
+  }
