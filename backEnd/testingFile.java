10c06212faea1b22f4b54a70aed4cc3e25d9ed67
MattBojanowski <BOJANOWM@email.sc.edu>
2024-03-19 19:18:05 -0400

    Need help importing JUnit

--- backEnd/testingFile.java
+++ backEnd/testingFile.java
@@ -1,2 +1,16 @@
 //testing file
 package backEnd;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class testingFile(){
+
+
+}
