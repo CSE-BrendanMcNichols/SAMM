cc215664bfb572f3c61727fba00b106f2517a2d2
MattBojanowski <BOJANOWM@email.sc.edu>
2024-03-21 11:42:47 -0400

    adding tests

--- backEnd/testingFile.java
+++ backEnd/testingFile.java
@@ -1,16 +1,21 @@
-//testing file
 package backEnd;
 
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
-class testingFile(){
-
+public class testingFile{
+    @Test
+    public void testCourseDefaultConstructor(){
+        Course c1 = new course();
+        
+    }
 
 }
